7d0300921398bc02b9f34d419151743d3ff30fa1
jackauk <jackauk@yahoo.com>
2019-08-29 22:51:24 +0700

    Merge branch 'master' of E:\Projects\AndroidFramework with conflicts.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_androidframework_MainActivity_stringFromJNI2(
+Java_com_example_androidframework_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
